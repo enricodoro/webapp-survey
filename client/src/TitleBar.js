1236751ee99f5c1d1dc66c35f949bc231b7c4fa3
Enrico D'Oro <er.enricodoro@gmail.com>
2021-06-19 16:51:14 +0200

    add mutual exclusive login feedback in navbar

--- client/src/TitleBar.js
+++ client/src/TitleBar.js
@@ -1,17 +1,74 @@
 import { GiWaveSurfer } from "react-icons/gi";
-import { useEffect, useState } from 'react'
-import { Container, Navbar, Button, Row } from 'react-bootstrap'
+import { AiOutlineUser } from "react-icons/ai";
+import { useState, useEffect } from 'react'
+import { Navbar, Button, Tooltip, OverlayTrigger, Nav } from 'react-bootstrap'
+import { LoginModal } from './Login.js'
+import API from './API.js'
 
 function TitleBar(props) {
-    return  <Navbar id="navbar">
-                <Navbar.Brand style={{ color: "crimson" }} href="/">
-                    <GiWaveSurfer id="logo" size="32"/>{' '}
-                    Surfeys
-                </Navbar.Brand>
-                <Button id="button">
-                    Login as admin
-                </Button>
-            </Navbar>
+    const [show, setShow] = useState(false)
+
+    const doLogIn = async (credentials) => {
+        try {
+            const userId = await API.logIn(credentials);
+            props.setLoggedIn(true);
+        } catch (err) {
+        }
+    }
+
+    const doLogOut = async () => {
+        await API.logOut();
+        props.setUsername('');
+        props.setLoggedIn(false);
+    }
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const user = await API.getAdmin()
+                props.setUsername(user.username);
+                props.setLoggedIn(true)
+            }
+            catch (err) {
+                console.error(err.error);
+            }
+        }
+        checkAuth();
+    }, [])
+
+    useEffect(() => {
+        if (props.loggedIn) {
+            API.getAdmin().then(user => { props.setUsername(user.username) })
+        }
+    }, [props.loggedIn])
+
+    return <Navbar id="navbar">
+        <Navbar.Brand style={{ color: "crimson" }} href="/">
+            <GiWaveSurfer id="logo" size="32" />{' '}
+            Surfeys
+        </Navbar.Brand>
+        {props.loggedIn ? <ShowUsername doLogOut={doLogOut} username={props.username} /> : <LoginButton setShow={setShow} />}
+        <LoginModal show={show} setShow={setShow} login={doLogIn} />
+    </Navbar>
+}
+
+function LoginButton(props) {
+    return <Button id="login" onClick={() => props.setShow(true)}>
+        Login as admin
+    </Button>
+}
+
+function ShowUsername(props) {
+    return <OverlayTrigger
+        placement="bottom"
+        delay={{ show: 250, hide: 400 }}
+        overlay={<Tooltip id="tooltip-logout">Logout</Tooltip>}
+    >
+        <Navbar.Brand onClick={() => props.doLogOut()}>
+            Welcome {props.username}!{' '}
+            <AiOutlineUser id="logo" size="32" />
+        </Navbar.Brand>
+    </OverlayTrigger>
 }
 
 export default TitleBar;
