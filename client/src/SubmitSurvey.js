ef79e8add9652edb7aa0d2ac6746f6783c75d5ec
Enrico D'Oro <er.enricodoro@gmail.com>
2021-06-23 18:09:47 +0200

    add save answers in db

--- client/src/SubmitSurvey.js
+++ client/src/SubmitSurvey.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { ListGroup, Col, Button, Form, Card } from 'react-bootstrap';
 import { QuestionCard } from './QuestionCard.js'
+import { Link } from 'react-router-dom'
 import API from './API';
 
 function SubmitSurvey(props) {
@@ -8,26 +9,31 @@ function SubmitSurvey(props) {
     const [givenAnswers, setGivenAnswers] = useState([])
     const [survey, setSurvey] = useState({})
     const [username, setUsername] = useState("")
-    const [id, setId] = useState(-1)
 
-    // const answer = {
-    //     idUser: ,
-    //     idQuestion: ,
-    //     idAnswer: ,
-    //     text: ,
-    // };
+    // givenAnswer = {qID: n, aID: n, text: string}
 
     const handleName = (e) => {
         setUsername(e)
     }
 
     const handleSubmit = () => {
-        const user = {
+        let user = {
             username: username,
             sID: props.surveyId
         }
         API.addUser(user).then(id => {
-            setId(id)
+            givenAnswers.forEach((a) => {
+                let answer = {
+                    id: id,
+                    sID: props.surveyId,
+                    qID: a.qID,
+                    aID: a.aID,
+                    text: a.text
+                }
+                API.addUserAnswer(answer).then(() => {
+
+                })
+            })
         })
     }
 
@@ -43,21 +49,25 @@ function SubmitSurvey(props) {
         })
     }, [props.surveyId])
 
-    return <Col className="d-flex flex-column mx-auto mt-3" style={{width: "60%"}}>
+    return <Col className="d-flex flex-column mx-auto mt-3" style={{ width: "60%" }}>
         <h1 className="text-center">{props.title}</h1>
         <h6 className="mandatory" >Mandatory questions are marked with</h6>
         <Card className="mx-auto" id="username-card">
             <Card.Body>
-            <Card.Title className="mandatory">Insert your name</Card.Title>
+                <Card.Title className="mandatory">Insert your name</Card.Title>
                 <Form>
                     <Form.Control maxLength="30" onChange={(e) => handleName(e.target.value)} placeholder="Insert your name or a username" />
                 </Form>
             </Card.Body>
         </Card>
         <ListGroup>
-            {questions.map((q, i) => <QuestionCard question={q} surveyId={props.surveyId} key={i} givenAnswers={givenAnswers} setGivenAnswers={setGivenAnswers}/>)}
+            {questions.map((q, i) => <QuestionCard question={q} surveyId={props.surveyId} key={i} givenAnswers={givenAnswers} setGivenAnswers={setGivenAnswers} />)}
         </ListGroup>
-        <Button id="button" className="mx-auto my-3" size="lg" onClick={()=>handleSubmit()}>Submit</Button>
+        <Link className="mx-auto my-3" style={{ textDecoration: 'none' }} to={{
+            pathname: "/"
+        }}>
+            <Button id="button" size="lg" onClick={() => handleSubmit()}>Submit</Button>
+        </Link>
     </Col>
 }
 
